140457ba11b2004ae31459abb94d0e60e13edd86
Markus Mannila <mmannila@abo.fi>
2017-08-19 15:44:29 +0300

    Functionality which fetches badges from Code School and displays them.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,34 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+	  url: 'https://www.codeschool.com/users/Crocostyle.json',
+	  datatype: 'jsonp',
+	  success: function(response) {
+		  response.courses.completed.forEach(function(course) {
+			  var badges = document.getElementById("badges");
+			  var newBadge = document.createElement("div");
+			  newBadge.classList.add("course");
+			  
+			  var title = document.createElement("h3");
+			  title.text = course.title;
+			  newBadge.appendChild(title);
+			  
+			  var img = document.createElement("img");
+			  img.setAttribute("src", course.badge);
+			  newBadge.appendChild(img); 
+
+			  var a = document.createElement("a");
+			  a.setAttribute("href", course.url);
+			  a.setAttribute("target", "_blank");
+			  a.classList.add("btn");
+			  a.classList.add("btn-primary");
+			  a.text = "See Course";
+			  
+			  newBadge.appendChild(a); 
+
+			  badges.appendChild(newBadge);
+		  });
+	  }
+  });
 
 });
